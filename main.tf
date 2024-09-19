a2ed937ab015b2c76904cf9cbb158a28357cc822
carla-gb <carla.paula@grupoboticario.com.br>
2024-09-19 16:03:40 -0300

    testando output

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+    cloud {
+    organization = "teste-carla"
+
+    workspaces {
+      name = "novo-workspace"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 data "aws_lb" "tech" {
     tags = {
         name = "kubernetes.io/service-name"
@@ -14,55 +40,55 @@ output "load_balancer_dns" {
 }
 
 
-# resource “aws_api_gateway_vpc_link” “main” {
-#  name = “tech_vpclink”
-#  description = “Foobar Gateway VPC Link. Managed by Terraform.”
-#  target_arns = load_balancer_arn
-# }
+resource “aws_api_gateway_vpc_link” “main” {
+ name = “tech_vpclink”
+ description = “Foobar Gateway VPC Link. Managed by Terraform.”
+ target_arns = load_balancer_arn
+}
 
-# resource “aws_api_gateway_rest_api” “main” {
-#  name = "tech_gateway"
-#  description = “Foobar Gateway used for EKS. Managed by Terraform.”
-#  endpoint_configuration {
-#    types = ["REGIONAL"]
-#  }
-# }
+resource “aws_api_gateway_rest_api” “main” {
+ name = "tech_gateway"
+ description = “Foobar Gateway used for EKS. Managed by Terraform.”
+ endpoint_configuration {
+   types = ["REGIONAL"]
+ }
+}
 
-# resource "aws_api_gateway_resource" "proxy" {
-#   rest_api_id = aws_api_gateway_rest_api.main.id
-#   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-#   path_part   = "{proxy+}"
-# }
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "{proxy+}"
+}
 
-# resource "aws_api_gateway_method" "proxy" {
-#   rest_api_id   = aws_api_gateway_rest_api.main.id
-#   resource_id   = aws_api_gateway_resource.proxy.id
-#   http_method   = "ANY"
-#   authorization = "NONE"
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
 
-#   request_parameters = {
-#     "method.request.path.proxy"           = true
-#     "method.request.header.Authorization" = true
-#   }
-# }
+  request_parameters = {
+    "method.request.path.proxy"           = true
+    "method.request.header.Authorization" = true
+  }
+}
 
-# resource "aws_api_gateway_integration" "proxy" {
-#   rest_api_id = aws_api_gateway_rest_api.main.id
-#   resource_id = aws_api_gateway_resource.proxy.id
-#   http_method = "ANY"
+resource "aws_api_gateway_integration" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = "ANY"
 
-#   integration_http_method = "ANY"
-#   type                    = "HTTP_PROXY"
-#   uri                     = "http://${load_balancer_dns}/{proxy}"
-#   passthrough_behavior    = "WHEN_NO_MATCH"
-#   content_handling        = "CONVERT_TO_TEXT"
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${load_balancer_dns}/{proxy}"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  content_handling        = "CONVERT_TO_TEXT"
 
-#   request_parameters = {
-#     "integration.request.path.proxy"           = "method.request.path.proxy"
-#     "integration.request.header.Accept"        = "'application/json'"
-#     "integration.request.header.Authorization" = "method.request.header.Authorization"
-#   }
+  request_parameters = {
+    "integration.request.path.proxy"           = "method.request.path.proxy"
+    "integration.request.header.Accept"        = "'application/json'"
+    "integration.request.header.Authorization" = "method.request.header.Authorization"
+  }
 
-#   connection_type = "VPC_LINK"
-#   connection_id   = aws_api_gateway_vpc_link.main.id
-# }
+  connection_type = "VPC_LINK"
+  connection_id   = aws_api_gateway_vpc_link.main.id
+}

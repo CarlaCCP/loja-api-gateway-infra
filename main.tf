9a92616f8375d13131e8c9b66b1d3cab32d41987
carla-gb <carla.paula@grupoboticario.com.br>
2024-09-19 15:25:33 -0300

    testando output

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-data "aws_lb" "tech" {
-#    filter {
-#     name = "tag:kubernetes.io/service-name"
-#     values = ["default/svc-loja"]
-#    }
-    most_recent = true
-}
-
-output "load_balancer_arn" {
-    value = data.aws_lb.tech.arn
-}
-
-output "load_balancer_dns" {
-    value = data.aws_lb.tech.dns
-}
-
 
 # resource “aws_api_gateway_vpc_link” “main” {
 #  name = “tech_vpclink”

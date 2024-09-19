d7856a26bf25d2e2638c64a4d34b22bc8b8dc41f
carla-gb <carla.paula@grupoboticario.com.br>
2024-09-19 19:35:29 -0300

    ajuste terraform

--- api-gateway.tf
+++ api-gateway.tf
@@ -44,7 +44,7 @@ resource "aws_api_gateway_integration" "proxy" {
 
   integration_http_method = "ANY"
   type                    = "HTTP_PROXY"
-  uri                     = "http://${data.aws_lbs.tech_lbs.lbs_name}/{proxy}"
+  uri                     = "http://${data.aws_lbs.tech_lbs}/{proxy}"
   passthrough_behavior    = "WHEN_NO_MATCH"
   content_handling        = "CONVERT_TO_TEXT"
 
